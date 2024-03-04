f4186c92a3ed13df425b706a695a150c6b4ad230
johltn <luttun.j@gmail.com>
2024-03-04 09:56:31 -0500

    refactor: externalize 3dverse functions from Canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,27 +1,8 @@
 import Script from "next/script";
-import { publicToken, mainSceneUUID } from "../utils/config.js";
 import { memo } from "react";
+import { initApp } from "../3dverse/helpers.js";
 
 export const Canvas = memo(({ onInputChange }) => {
-    const initApp = async () => {
-        await SDK3DVerse.joinOrStartSession({
-            userToken: publicToken,
-            sceneUUID: mainSceneUUID,
-
-            canvas: document.getElementById("display-canvas"),
-            viewportProperties: {
-                defaultControllerType: SDK3DVerse.controller_type.editor,
-            },
-        });
-
-        const projectEntity = (await SDK3DVerse.engineAPI.findEntitiesByNames("IfcProject"))[0];
-        const projectGlobalCenter = projectEntity.getGlobalAABB().center;
-
-        SDK3DVerse.updateControllerSetting({
-            lookAtPoint: [projectGlobalCenter[0], projectGlobalCenter[1], projectGlobalCenter[2]],
-        });
-    };
-
     const handleContextMenu = (event: any) => {
         event.preventDefault();
     };
