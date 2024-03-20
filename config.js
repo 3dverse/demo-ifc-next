90edfa766743ebe171b2c9359ccff82bd2084094
Mark Downey <mark@3dverse.com>
2024-03-20 11:08:05 -0400

    chore: add config for demo

--- config.js
+++ config.js
@@ -1,12 +1,2 @@
-// Find your Public Token in the 3dverse Console (https://console.3dverse.com):
-// - Go to the API Access page.
-// - Copy the Public Token and paste it below.
-
-export const publicToken = "public_TKysjsYtTdahW699";
-
-// Replace asset UUIDs:
-// - Go to the Asset Browser page.
-// - Double click on the Public folder to open it.
-// - Select the Main Scene and copy the Asset UUID to replace it below.
-
-export const mainSceneUUID = "e8650680-0933-4a4f-9ad1-983de50433e3";
+export const publicToken = 'public_TKysjsYtTdahW699';
+export const mainSceneUUID = 'ab32e8dd-b8aa-4125-9caa-0d22cb125980';
