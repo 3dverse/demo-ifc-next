a03187b4cfcc90b4c8b2dee6d18b27fff2104008
Mark Downey <mark@3dverse.com>
2024-03-20 11:07:36 -0400

    chore: add github pages build script

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/demo-ifc-next',
 };
 
 module.exports = nextConfig;
