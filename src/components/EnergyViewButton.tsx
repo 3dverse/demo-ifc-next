c50102d96d75d9442b535a55d035405e47b04dad
johltn <luttun.j@gmail.com>
2024-03-29 04:42:17 -0400

    fix: more informative text on energy consumption view transitions

--- src/components/EnergyViewButton.tsx
+++ src/components/EnergyViewButton.tsx
@@ -29,7 +29,7 @@ export const EnergyViewButton = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="icon">
                             <path d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zm74.1-396.4c5.8 4.7 7.6 12.9 4.2 19.6L281.9 240H352c6.8 0 12.9 4.3 15.1 10.7s.2 13.5-5.1 17.8l-160 128c-5.9 4.7-14.2 4.7-20.1-.1s-7.6-12.9-4.3-19.6L230.1 272H160c-6.8 0-12.8-4.3-15.1-10.7s-.2-13.5 5.1-17.8l160-128c5.9-4.7 14.2-4.7 20.1.1z" />
                         </svg>
-                        {energyVisible ? "Hide Energy Consumption" : "Visualize Energy Consumption"}
+                        {energyVisible ? "Hiding Energy Consumption..." : "Showing Energy Consumption..."}
                     </button>
                     <Spinner color="purple.500" />
                 </div>
