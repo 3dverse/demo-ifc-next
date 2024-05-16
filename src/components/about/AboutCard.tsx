05d0a691fe972d1b6ab80202f625cb7ec874f859
Baptiste Macaire <baptiste@3dverse.com>
2024-05-16 00:11:47 -0400

    style: remove 3dverse bottom badge on mobile

--- src/components/about/AboutCard.tsx
+++ src/components/about/AboutCard.tsx
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-import { Card } from "@chakra-ui/react";
+import { Button, Card } from "@chakra-ui/react";
 
 //------------------------------------------------------------------------------
 import Logo3dverse from "../../../public/socials/logo_3dverse.svg";
@@ -8,25 +8,18 @@ import Logo3dverse from "../../../public/socials/logo_3dverse.svg";
 //------------------------------------------------------------------------------
 export const AboutCard = () => {
     return (
-        <aside className="md:hidden fixed bottom-0 w-full p-4">
-            <Card
+        <aside className="md:hidden fixed bottom-0 left-0 p-4">
+            <Button
                 as="a"
-                variant="glass"
-                mx="auto"
-                w="max-content"
-                pl={4}
-                pr={6}
-                py={3}
-                rounded="full"
                 target="_blank"
                 href="https://3dverse.com/"
-                bgColor="#ffc700"
+                p={2}
+                bgColor="#000000"
+                border="none"
+                rounded="full"
             >
-                <div className="flex justify-center items-center gap-3">
-                    <Logo3dverse className="w-6 brightness-0" />
-                    <p className="text-center text-sm text-secondary font-medium">Application powered by 3dverse.</p>
-                </div>
-            </Card>
+                <Logo3dverse className="w-6 " />
+            </Button>
         </aside>
     );
 };
